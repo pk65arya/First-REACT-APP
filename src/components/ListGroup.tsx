2fb7783e5f09e6534d737fa18287d594bf1e2d07
pk65arya <pk65arya@gmail.com>
2024-06-10 12:40:55 +0530

    event management

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,10 +1,19 @@
-import { MouseEvent } from "react";
+//import { MouseEvent } from "react";
+
+import { useState } from "react";
 
 function ListGroup() {
   let items = ["Ney York", "San francisco", "Tokyo", "London", "Paris"];
+  //  let selectedIndex = 0;
 
   //Event handler
-  const handleClick = (event: MouseEvent) => console.log(event);
+  // const handleClick = (event: MouseEvent) => console.log(event);
+
+  //hook
+  const [selectedIndex, setSelectedIndex] = useState(-1);
+  //  const [name,setName]=useState('');
+  //arr[0] //variable(selectedIndex)
+  //arr[1] //updater function
   return (
     <>
       <h1>List</h1>
@@ -12,7 +21,17 @@ function ListGroup() {
 
       <ul className="list-group">
         {items.map((item, index) => (
-          <li className="list-group-item" key={item} onClick={handleClick}>
+          <li
+            className={
+              selectedIndex === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+            key={item}
+            onClick={() => {
+              setSelectedIndex(index);
+            }}
+          >
             {item}
           </li>
         ))}
